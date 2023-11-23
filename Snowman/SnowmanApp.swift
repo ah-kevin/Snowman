c54840ce264a9203ead33f7c77af0d8223865537
bjke <kbjwdh@gmail.com>
2023-11-23 22:39:25 +0800

    Initial Commit

---
+++ Snowman/SnowmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowmanApp.swift
+//  Snowman
+//
+//  Created by bjke on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
